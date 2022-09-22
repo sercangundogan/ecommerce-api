f954aa350c57a2ec1b8b3e96356c58a72f22bbf0
Sercan Gündoğan <sercan@sociality.io>
2022-09-22 18:35:31 +0300

    Added one to many and many to one relationships

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const products = [
         price: 42.44,
         image: "img-1",
         onSale: false,
+        categoryId: "34115aac-0ff5-4859-8f43-10e8db23602b",
     },
     {
         id: "c2af9adc-d0b8-4d44-871f-cef66f86f7f6",
@@ -23,6 +24,7 @@ const products = [
         price: 53.5,
         image: "img-2",
         onSale: false,
+        categoryId: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
     },
     {
         id: "2c931e7e-510f-49e5-aed6-d6b44087e5a1",
@@ -32,6 +34,7 @@ const products = [
         price: 1.33,
         image: "img-3",
         onSale: true,
+        categoryId: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
     },
     {
         id: "404daf2a-9b97-4b99-b9af-614d07f818d7",
@@ -41,6 +44,7 @@ const products = [
         price: 332,
         image: "img-4",
         onSale: false,
+        categoryId: "34115aac-0ff5-4859-8f43-10e8db23602b",
     },
     {
         id: "6379c436-9fad-4b3f-a427-2d7241f5c1b1",
@@ -50,6 +54,7 @@ const products = [
         price: 23.11,
         image: "img-5",
         onSale: true,
+        categoryId: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
     },
     {
         id: "f01bcdec-6783-464e-8f9e-8416830f7569",
@@ -59,6 +64,7 @@ const products = [
         price: 59.99,
         image: "img-6",
         onSale: true,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d",
     },
     {
         id: "a4824a31-5c83-42af-8c1b-6e2461aae1ef",
@@ -68,6 +74,7 @@ const products = [
         price: 427.44,
         image: "img-7",
         onSale: false,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d",
     },
     {
         id: "b553085a-a7e0-4c9b-8a12-f971919c3683",
@@ -77,6 +84,7 @@ const products = [
         price: 77.0,
         image: "img-8",
         onSale: true,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d",
     },
     {
         id: "47bf3941-9c8b-42c0-9c72-7f3985492a5b",
@@ -86,6 +94,7 @@ const products = [
         price: 93.44,
         image: "img-9",
         onSale: false,
+        categoryId: "d914aec0-25b2-4103-9ed8-225d39018d1d",
     },
 ]
 
@@ -136,11 +145,13 @@ const typeDefs = gql`
         image: String!
         price: Float!
         onSale: Boolean!
+        category: Category
     }
 
     type Category {
         id: ID!
         name: String
+        products: [Product!]!
     }
 `
 
@@ -181,6 +192,23 @@ const resolvers = {
         category: (parent, args, context) => {
             const { id } = args
             return categories.find((category) => category.id === id)
+        },
+    },
+
+    Category: {
+        products: (parent, args, context) => {
+            const categoryId = parent.id
+            return products.filter(
+                (product) => product.categoryId === categoryId
+            )
+        },
+    },
+
+    Product: {
+        category: (parent, args, context) => {
+            console.log(parent)
+            const { categoryId } = parent
+            return categories.find((category) => category.id === categoryId)
         },
     },
 }
